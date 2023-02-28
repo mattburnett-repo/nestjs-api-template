1b195ac60345df09e4577711a1a74062de7aa627
Matt Burnett <mburnett@tutanota.de>
2023-02-28 17:09:41 +0100

    fix: typo in database var name

--- src/config/dbConfig.ts
+++ src/config/dbConfig.ts
@@ -9,7 +9,7 @@ export default registerAs('database', () => {
     port: +process.env.DB_PORT,
     username: process.env.DB_USER_NAME,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABSE_NAME,
+    database: process.env.DB_DATABASE_NAME,
     autoLoadEntities: true,
     entities: [__dirname + '/../**/*.entity{.ts,.js}'],
     // typeorm / synchornization logging:
